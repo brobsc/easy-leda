9ed844bf5f8e15debff33513ae616cedc8995a8c
Rob <bruno.robsiq@gmail.com>
2019-05-07 09:19:54 -0300

    Add user-conf reading and updating

--- src/easy_leda/core.clj
+++ src/easy_leda/core.clj
@@ -1,5 +1,30 @@
 (ns easy-leda.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :as pp]))
+
+(def user-file-path [(System/getProperty "user.home") ".easy_leda"])
+(def user-file (apply io/file user-file-path))
+
+(def user-conf (read-string (slurp (doto (apply io/file user-file-path)))))
+
+(defn prepare-user-file []
+  (when-not (.exists (apply io/file user-file-path))
+    (spit (apply io/file user-file-path) "")))
+
+(defn update-conf [mat]
+  (spit user-file (prn-str {:mat mat})))
+
+(defn read-conf []
+  (print "Insira sua matricula: ")
+  (flush)
+  (let [mat (read-line)]
+    (update-conf mat)))
+
+(defn start []
+  (prepare-user-file)
+  (println user-conf))
 
 (defn -main []
-  (println "Hello World!"))
+  (println "Hello World!")
+  (read-conf))
