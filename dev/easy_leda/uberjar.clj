78f2c429818ede013b21db5a51a2fca9898d58ab
Rob <bruno.robsiq@gmail.com>
2019-06-19 08:32:32 -0300

    Add experimental build command

--- dev/easy_leda/uberjar.clj
+++ dev/easy_leda/uberjar.clj
@@ -1,17 +1,21 @@
 (ns easy-leda.uberjar
   (:require [cambada.uberjar :as u]
+            [cambada.jar :as jar]
+            [cambada.cli :as ccli]
             [clojure.java.io :as io]
             [easy-leda.core :as e]))
 
 (defn -main []
   (println "Buildando versao" e/-version)
-  (u/-main "--app-version" e/-version
-            "-m" "easy_leda.core")
+  (let [task (ccli/args->task ["--app-version" e/-version
+                               "-m" "easy_leda.core"]
+                              jar/cli-options)]
+    (u/apply! task))
   (println "Movendo jar")
   (-> (io/file (System/getProperty "user.dir")
-                "target/"
-                "easy-leda-"
-                e/-version
-                "-standalone.jar")
+               "target/"
+               (str "easy-leda-"
+               e/-version
+               "-standalone.jar"))
       (io/copy (io/file (System/getProperty "user.dir") "easy-leda.jar"))))
 
